06160a228c74e3a919a488d6220cfee12e919e7c
Felipe Gaete <felipe.r.gaete.a@gmail.com>
2020-06-06 23:56:48 -0400

    Componentes (heroes)

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Magneto' },
+  { id: 12, name: 'Cyclops' },
+  { id: 13, name: 'QuickSilver' },
+  { id: 14, name: 'Wolverine' },
+  { id: 15, name: 'Beast' },
+  { id: 16, name: 'Rogue' },
+  { id: 17, name: 'Jubilee' },
+  { id: 18, name: 'Emma Frost' },
+  { id: 19, name: 'Gambit' },
+  { id: 20, name: 'Captain America' }
+];
